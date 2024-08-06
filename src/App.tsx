99efb8d01bb3bb2576a0a367e8657ba2a8717ea4
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 17:36:10 -0600

    [FEAT] Envolviendo el retorno del componente App por el elemento html main

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <main>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </>
+    </main>
   )
 }
 
