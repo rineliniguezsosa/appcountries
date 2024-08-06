b92981e1e7a9f456828c63928d3a95b79e9594a1
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 17:59:18 -0600

    [FEAT] Cambios en el archivo App.tsx
    - importando el componente CountriesDetail
    -  estableciendo el componente Route para la definición de la ruta y el parametro name "/countries/detail/:name" para obsrevar los detalles de los paises, en esta ruta devolveremos el componente CountriesDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes,Route } from 'react-router-dom';
 import { Countries } from './pages/Countries';
+import { CountriesDetail } from './pages/CountriesDetail';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <main>
         <Routes>
           <Route path='/' element={<Countries/>} />
+          <Route path='/countries/detail/:nane' element={<CountriesDetail/>} />
         </Routes>
     </main>
   )
