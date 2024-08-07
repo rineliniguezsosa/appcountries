1f199a2b58539f10684fee3d2d2e314ffcad4318
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 19:55:27 -0600

    [FIX] Limite la escritura del componente DarkModeBtn me agrada que el componente sea definiendo de una menara corta

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,7 +7,7 @@ export const Nav = () => {
         <nav className="w-full px-7 py-7 border-2 border-blue-900">
             <h1>Where in the world?</h1>
 
-            <DarkModeBtn></DarkModeBtn>
+            <DarkModeBtn/>
         </nav>
     </header>
   )
