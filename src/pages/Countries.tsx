66fefd8aa64085b835d45391b795f50957199edf
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 11:43:55 -0600

    [FEAT] Guardando temporalmente los cambios en el archivo Countries.tsx
    - Importando el componente MuiFormControl
    - estableciendo el componente MuiFormControl en el retorno del componente Countries
    - estableciendo las props variant de tipo outlined y la prop fullWidth en true en el componente MuiFormControl
    - guardando el componente InputLabel, el componente InputLabel es la descripción del input, este sera claro un componente
    - guardando los cambios desayuno rapido y le seguimos, claro que si

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -2,16 +2,23 @@ import {
   FormControl,IconButton, InputAdornment, InputLabel, OutlinedInput, Select,
    MenuItem, SelectChangeEvent
 } from "@mui/material";
+import { MuiFormControl } from "../components/MuiFormControl";
 
 export const Countries = () => {
   return (
     <div className="w-full h-screen border-2 border-red-900 px-7 py-7">
       <form>
            {/* inicio FormControl */}
-           <FormControl variant="outlined" className='w-full'>
-                    
-                  
-           </FormControl>
+           <MuiFormControl variant="outlined" fullWidth={true}>
+                    <InputLabel 
+                    className="text-LightModeDarkGrayInput" 
+                    id="country-name" 
+                    htmlFor="outlined-adornment-search"
+                    >
+                      Search for a country...
+                    </InputLabel>
+      
+           </MuiFormControl>
            {/* fin FormControl */}
       </form>
     </div>
