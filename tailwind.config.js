c42dd2115727b98cba6298f959ad9df7d56298d0
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 18:29:47 -0600

    [FEAT] Configurando los colores personalizados en el archivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        DarkBlueBackground:'hsl(209, 23%, 22%)', // Dark Blue (Dark Mode Elements)
+        DarkModeBg: 'hsl(207, 26%, 17%)',//Very Dark Blue (Dark Mode Background)
+        LightModeTextDarkBlue: 'hsl(200, 15%, 8%)', //Very Dark Blue (Light Mode Text)
+        LightModeDarkGrayInput:'hsl(0, 0%, 52%)', // Dark Gray (Light Mode Input)
+        LightModeBgGray: 'hsl(0, 0%, 98%)' // Very Light Gray (Light Mode Background)
+      }
+    },
   },
   plugins: [],
 }
