201363f5ba7b4f0db4b993f667a52f65322a22fb
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 19:43:08 -0600

    [FEAT] Creamos el hook useState donde inicializanmos el state con false
    - inicializamos la variable darkmode en false
    - bueno setdarkmode es una función el cual actualiza el state

--- src/components/DarkModeBtn.tsx
+++ src/components/DarkModeBtn.tsx
@@ -3,6 +3,7 @@ import { useEffect,useState } from "react"
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 
 export const DarkModeBtn = () => {
+  const [darkmode, setdarkmode] = useState(false);
   return (
     <MuiButton variant="text" title="Dark mode" icon={<DarkModeOutlinedIcon/>}/>      
   )
