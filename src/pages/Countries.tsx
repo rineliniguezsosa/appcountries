977dda8d9f0b5a12f47077802b3fcbf2394902c0
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 20:42:59 -0600

    [FEAT] Estableciendo el elemento html form en el archivo Countries.tsx

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -3,7 +3,9 @@
 export const Countries = () => {
   return (
     <div className="w-full h-screen border-2 border-red-900 px-7 py-7">
-      Countries
+      <form>
+
+      </form>
     </div>
   )
 }
