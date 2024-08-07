7cda27cd1409dee8cd8bb4d4a884e65f13741fa3
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 20:13:10 -0600

    [FEAT] Establecemos la clase dark y obsrevamos los pequeños cambios establecidos para observar el total funcionamiento del darkmode

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,9 @@ import { DarkModeBtn } from "./DarkModeBtn"
 
 export const Nav = () => {
   return (
-    <header className="w-full border-2 border-red-600">
+    <header className="w-full border-2 border-red-600 dark:border-yellow-600">
         <nav className="w-full px-7 py-7 border-2 border-blue-900">
-            <h1>Where in the world?</h1>
+            <h1 className="text-black dark:text-white">Where in the world?</h1>
 
             <DarkModeBtn/>
         </nav>
