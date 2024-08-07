52b032deecf7039cebe4af9637d180004de65c17
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 13:38:36 -0600

    [FEAT] Cambios en el archivo Countries.tsx
    - Importando el componente MuiOutlinedInput
    - importando el icono para el input SearchIcon
    - retornando el componente MuiOutlinedInput en el componente Countries
    - estableciendo las props id con el valor de outlined-adornment-search para que coincida con el htmlFor del MuinputLabel, type="text" y startAdornment para establecer el icono al principio, label con el valor de  "Search for a country..."

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -4,6 +4,8 @@ import {
 } from "@mui/material";
 import { MuiFormControl } from "../components/MuiFormControl";
 import { MuiInputLabel } from "../components/MuiInputLabel";
+import { MuiOutlinedInput } from "../components/MuiOutlinedInput";
+import SearchIcon from '@mui/icons-material/Search';
 
 export const Countries = () => {
   return (
@@ -18,6 +20,20 @@ export const Countries = () => {
                     >
                       Search for a country...
                     </MuiInputLabel>
+
+                    <MuiOutlinedInput
+                        id="outlined-adornment-search"
+                        fullWidth
+                        type="text"
+                        startAdornment={
+                          <InputAdornment position="start">
+                            <IconButton edge="start">
+                                    <SearchIcon/>
+                            </IconButton>
+                          </InputAdornment>
+                        }
+                        label="Search for a country..."
+                      />
       
            </MuiFormControl>
            {/* fin FormControl */}
