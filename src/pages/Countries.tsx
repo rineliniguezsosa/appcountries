bddf792a370b71537a85a497fd3ad81fd858dd51
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 20:50:46 -0600

    [FEAT] Estableciendo el componente FormControl de material ui
    - establecemos la variante como outlined y le establecemos un width full para que ocupe todo el ancho del contenedor
    - voy a dejarlo hasta aqui estoy cansado y me duele la espalda mañana le seguimos claro que si ;)

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -7,7 +7,12 @@ export const Countries = () => {
   return (
     <div className="w-full h-screen border-2 border-red-900 px-7 py-7">
       <form>
-
+           {/* inicio FormControl */}
+           <FormControl variant="outlined" className='w-full'>
+                    
+                  
+           </FormControl>
+           {/* fin FormControl */}
       </form>
     </div>
   )
