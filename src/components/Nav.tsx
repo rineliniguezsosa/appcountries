38f7ee86036658b6ba2a2873452dbacc32ac6a9c
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 20:25:04 -0600

    [FEAT] Usamos flex-box en el contenedor nav para alinear los elementos en fila y para alienar los elementos entre el spacio del contenedor, elementos en el centro mediante items-center

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,7 @@ import { DarkModeBtn } from "./DarkModeBtn"
 export const Nav = () => {
   return (
     <header className="w-full border-2 border-red-600 dark:border-yellow-600">
-        <nav className="w-full px-7 py-7 border-2 border-blue-900">
+        <nav className="w-full px-7 py-7 border-2 border-blue-900 flex flex-row justify-between items-center">
             <h1 className="text-black dark:text-white">Where in the world?</h1>
 
             <DarkModeBtn/>
