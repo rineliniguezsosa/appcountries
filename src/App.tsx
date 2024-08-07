1b10e3a8c9bc18fe8bf544d37a80640669fbfc07
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 18:02:36 -0600

    [FEAT] Cambios en el archivo App.tsx
    - importando el componente Nav
    - estableciendo el componente Nav en la devolución del funcional componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from 'react-router-dom';
 import { Countries } from './pages/Countries';
 import { CountriesDetail } from './pages/CountriesDetail';
+import { Nav } from './components/Nav';
 import './App.css'
 
 function App() {
 
   return (
     <main>
+      <Nav/>
         <Routes>
           <Route path='/' element={<Countries/>} />
           <Route path='/countries/detail/:nane' element={<CountriesDetail/>} />
