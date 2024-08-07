69ce7b3f7c3ee59b508f8317780d50970a8fb71e
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 19:54:05 -0600

    [FEAT] Estableciendo el componente DarkModeBtn en el retorno del funcional component Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,4 @@
+import { DarkModeBtn } from "./DarkModeBtn"
 
 
 export const Nav = () => {
@@ -5,6 +6,8 @@ export const Nav = () => {
     <header className="w-full border-2 border-red-600">
         <nav className="w-full px-7 py-7 border-2 border-blue-900">
             <h1>Where in the world?</h1>
+
+            <DarkModeBtn></DarkModeBtn>
         </nav>
     </header>
   )
