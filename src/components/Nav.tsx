a8fee707be4a23828f8a4f06d5e077e98a4fe391
Rinel <rineliniguezsosa@gmail.com>
2024-08-06 18:27:48 -0600

    [FEAT] Cambios en el archivo Nav.ts
    -  css en el elemento header, width-full para que ocupe todo el ancho, tamaño y color de borde
    - css en el elemento nav, width-full para que ocupe todo el ancho, padding izquierdo y derecho 7rem, padding de arriba y abajo 7ren, color y tamaño de borde

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,8 +2,10 @@
 
 export const Nav = () => {
   return (
-    <header>
-
+    <header className="w-full border-2 border-red-600">
+        <nav className="w-full px-7 py-7 border-2 border-blue-900">
+            <h1>Where in the world?</h1>
+        </nav>
     </header>
   )
 }
