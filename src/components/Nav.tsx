ce5f7d170ed04547ad3c0c36730f73ec1e2de022
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 12:39:07 -0600

    [FEAT] Estableciendo css en el elemento h1
    - estableciendo al elmento h1 la fuente nunito sans en extra bold
    - texto en tamaño base a 16px usando el text-base

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ export const Nav = () => {
   return (
     <header className="w-full shadow-md dark:border-yellow-600">
         <nav className="w-full px-7 py-7  flex flex-row justify-between items-center">
-            <h1 className="text-black dark:text-white">Where in the world?</h1>
+            <h1 className="font-sans font-extrabold text-base dark:text-white">Where in the world?</h1>
 
             <DarkModeBtn/>
         </nav>
