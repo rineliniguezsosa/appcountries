b3ba81e79a098a0f6370a8fb746bae2d534d90a8
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 21:54:10 -0600

    [FEAT] Cambios en el archivo CountryCard.tsx
    - moviendo el componente Card como el componente principal y estableciendo css como tamaño y color de borde, width al 75% mediante el w-3/4
    - estableciendo la prop css text-decoration:none mediante no-underline al componente Link
    - estableciendo formato al componente CountryCard

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -4,32 +4,32 @@ import { Card, CardContent, CardMedia } from "@mui/material"
 
 export const CountryCard = ({flag,name,population,region,capital}:CountryCardProps) => {
   return (
-    <Link to={`/countries/dertail/${name}`}>
-        <Card>
-            <CardMedia
-                sx={{ height: 140 }}
-                image={flag}
-                title="flags"
-            />
+        <Card className="border-2 border-blue-600 w-3/4">
+            <Link to={`/countries/detail/${name}`} className="no-underline">
+                <CardMedia
+                    sx={{ height: 140 }}
+                    image={flag}
+                    title="flags"
+                />
 
-            <CardContent className="dark:bg-DarkBlueBackground">
-                <h1 className="font-sans-extrabold font-bold mb-1 dark:text-white">
-                    {name}
-                </h1>
-                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                    Population:
-                    <span className="text-LightModeDarkGrayInput">{population}</span>
-                </p>
-                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                    Region:
-                    <span className="text-LightModeDarkGrayInput">{region}</span>
-                </p>
-                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                    Capital:
-                    <span className="text-LightModeDarkGrayInput">{capital}</span>
-                </p>
-            </CardContent>
+                <CardContent className="dark:bg-DarkBlueBackground">
+                    <h1 className="font-sans-extrabold font-bold mb-1 dark:text-white">
+                        {name}
+                    </h1>
+                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                        Population:
+                        <span className="text-LightModeDarkGrayInput">{population}</span>
+                    </p>
+                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                        Region:
+                        <span className="text-LightModeDarkGrayInput">{region}</span>
+                    </p>
+                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                        Capital:
+                        <span className="text-LightModeDarkGrayInput">{capital}</span>
+                    </p>
+                </CardContent>
+            </Link>
         </Card>
-    </Link>
   )
 }
