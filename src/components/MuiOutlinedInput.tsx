ddbd2e9447c4d80edd191449cb9a84211a5039d6
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 18:12:10 -0600

    [FEAT] Estableciendo la prop onChange en el componente reusable MuiOutlinedInput
    - desestructurando el argumento onChange
    - pasando el evento onChange a la prop onChange del componente OutlinedInput

--- src/components/MuiOutlinedInput.tsx
+++ src/components/MuiOutlinedInput.tsx
@@ -2,8 +2,15 @@ import { OutlinedInput } from "@mui/material"
 import { MuiOutlinedInputProps } from "../types"
 
 
-export const MuiOutlinedInput = ({id,fullWidth,type,startAdornment,label}:MuiOutlinedInputProps) => {
+export const MuiOutlinedInput = ({id,fullWidth,type,startAdornment,label,onChange}:MuiOutlinedInputProps) => {
   return (
-    <OutlinedInput id={id} fullWidth={fullWidth} type={type} startAdornment={startAdornment} label={label}/>
+    <OutlinedInput 
+      id={id}
+      fullWidth={fullWidth}
+      type={type} 
+      startAdornment={startAdornment} 
+      label={label} 
+      onChange={onChange}
+    />
   )
 }
