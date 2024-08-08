4b6f4f8fc8af84924959e3fd35748a3528ec9284
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 21:36:12 -0600

    [FIX] Removiendo la prop h-screen en el div principal de la pagina Countries donde se encargaba esa propieda de ocupar todo el alto de la pantalla

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -69,7 +69,7 @@ export const Countries = () => {
   }
 
   return (
-    <div className="w-full h-screen border-2 border-red-900 px-7 py-7">
+    <div className="w-full border-2 border-red-900 px-7 py-7">
       <form className="flex flex-col gap-7 border-2 border-red-500">
            {/* inicio FormControl */}
            <MuiFormControl variant="outlined" fullWidth={true}>
