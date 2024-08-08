b0296107147a9186459eda9c4d667d5772c88a89
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 16:53:39 -0600

    [FEAT] Cambios en el archivo CountriesDetail
    - Importando el hook useNavigate para la navegación programada
    - creando la variable navigate el cual fue asignada la referemcia de la función useNavigate
    - creé la arrow function returnCountriespage para regresar a la pagina inicial  mediante navigate, como bien ya se la barra en diagonal especifica la ruta principal "/"
    - devolvemos en el retorno del componente CountriesDetail el componente MuiButton
    - establecemos las props del componente MuiButton
    - prop onClick donde pase por referencia el método returnCountriespage
    - prop variant outlined para el tipo del botón de material ui
    - prop title para el titulo del botón como "Back"
    - prop icon con el valor del componente WestIcon

--- src/pages/CountriesDetail.tsx
+++ src/pages/CountriesDetail.tsx
@@ -1,11 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import { MuiButton } from "../components/MuiButton"
 import WestIcon from '@mui/icons-material/West';
 
 
 export const CountriesDetail = () => {
+  const navigate = useNavigate();
+  console.log(navigate);
+
+  const returnCountriespage = () =>{
+    navigate('/')
+  }
+  
   return (
     <div className="w-full border-2 border-red-300">
-
+      <MuiButton onClick={returnCountriespage} variant="outlined" title="Back" icon={<WestIcon/>} />
     </div>
   )
 }
