e4177d5b65a7540518abe4e24534fc474dafe1b0
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 22:13:27 -0600

    [FEAT] Estableciendo una sombra mediante mediante la prop shadow-md de tailwindcss
    - removiendo los tamaño y colores de borde para observar la sombra

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,8 +3,8 @@ import { DarkModeBtn } from "./DarkModeBtn"
 
 export const Nav = () => {
   return (
-    <header className="w-full border-2 border-red-600 dark:border-yellow-600">
-        <nav className="w-full px-7 py-7 border-2 border-blue-900 flex flex-row justify-between items-center">
+    <header className="w-full shadow-md dark:border-yellow-600">
+        <nav className="w-full px-7 py-7  flex flex-row justify-between items-center">
             <h1 className="text-black dark:text-white">Where in the world?</h1>
 
             <DarkModeBtn/>
