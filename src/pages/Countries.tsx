b0a40dd89ec2b9eca01c6288254662b55b11ccad
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 18:57:28 -0600

    [FEAT] Convirtiendo en minúscula la prop common ya que no iba a coincidir la busqueda entre mayúsculas y minúsculas
    - voy a dejarlo hasta aqui, tomar un break y necesito hacer ejercicio pero terminando le regresamos

--- src/pages/Countries.tsx
+++ src/pages/Countries.tsx
@@ -37,7 +37,7 @@ export const Countries = () => {
     let filteredCountry = countriescopy;
 
     if(countryname){
-      filteredCountry = filteredCountry.filter(country => country.name.common.includes(countryname.toLowerCase()))
+      filteredCountry = filteredCountry.filter(country => country.name.common.toLowerCase().includes(countryname.toLowerCase()))
     }
     console.log("filter country",filteredCountry);
     
