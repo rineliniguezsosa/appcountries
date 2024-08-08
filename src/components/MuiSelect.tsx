dc913ac28d889e7a9e220f3a904f1bed8b3f0ebf
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 12:00:01 -0600

    [FEAT] Estableciendo la prop className en el componente MuiSelect
    - desestructurando la prop className
    - estableciendo la prop className en el componente Select

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,8 +1,8 @@
 import { Select } from "@mui/material"
 import {MuiSelectProps} from '../types'
 
-export const MuiSelect = ({labelId, id, fullWidth, label, children,onChange }:MuiSelectProps) => {
+export const MuiSelect = ({labelId, id, fullWidth, className, label, children,onChange }:MuiSelectProps) => {
   return (
-    <Select labelId={labelId} id={id} fullWidth={fullWidth} label={label} onChange={onChange}>{children}</Select>
+    <Select labelId={labelId} id={id} fullWidth={fullWidth} className={className} label={label} onChange={onChange}>{children}</Select>
   )
 }
