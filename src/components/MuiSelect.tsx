5a195669af7a0951afa21f29007a9d88d495898b
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 18:19:19 -0600

    [FEAT] Establecemos la prop onChange en el componente MuiSelect
    - desestructuramos el argumento onChange
    - establecemos a la prop onChange del componente Select el valor del argumento onChange

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,8 +1,8 @@
 import { Select } from "@mui/material"
 import {MuiSelectProps} from '../types'
 
-export const MuiSelect = ({labelId, id, fullWidth, label, children }:MuiSelectProps) => {
+export const MuiSelect = ({labelId, id, fullWidth, label, children,onChange }:MuiSelectProps) => {
   return (
-    <Select labelId={labelId} id={id} fullWidth={fullWidth} label={label}>{children}</Select>
+    <Select labelId={labelId} id={id} fullWidth={fullWidth} label={label} onChange={onChange}>{children}</Select>
   )
 }
