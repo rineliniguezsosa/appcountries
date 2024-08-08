18763739699cf13fe1dd97ea4314e81a87ba8b88
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 21:17:13 -0600

    [FEAT] Pegando el contenido de mi CountryCard
    - estableci el componente Card
    - estableci el componente CardMedia con las props image con el valor de la prop flag, estableci el title de la imagen
    - estableci el componente CardContent donde estableci la clase dark, entonces cuando realizamos el toggle de la clase dark cambiara el color de fondo
    - estableciendo los elementos html h1, p y span

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -2,8 +2,34 @@ import { Link } from "react-router-dom"
 import { CountryCardProps } from '../types'
 import { Card, CardContent, CardMedia } from "@mui/material"
 
-export const CountryCard = ({id,flag,name,population,region,capital}:CountryCardProps) => {
+export const CountryCard = ({flag,name,population,region,capital}:CountryCardProps) => {
   return (
-    <Link to={`/countries/dertail/${name}`}>CountryCard</Link>
+    <Link to={`/countries/dertail/${name}`}>
+        <Card>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={flag}
+                title="flags"
+            />
+
+            <CardContent className="dark:bg-DarkBlueBackground">
+                <h1 className="font-sans-extrabold font-bold mb-1 dark:text-white">
+                    {name}
+                </h1>
+                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                    Population:
+                    <span className="text-LightModeDarkGrayInput">{population}</span>
+                </p>
+                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                    Region:
+                    <span className="text-LightModeDarkGrayInput">{region}</span>
+                </p>
+                <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
+                    Capital:
+                    <span className="text-LightModeDarkGrayInput">{capital}</span>
+                </p>
+            </CardContent>
+        </Card>
+    </Link>
   )
 }
