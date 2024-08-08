c4f4487013c1de37e0208b3d4619e41e7bb67dda
Rinel <rineliniguezsosa@gmail.com>
2024-08-07 21:03:15 -0600

    [FEAT] Importando el componente CardContent en el componente CountryCard

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { CountryCardProps } from '../types'
-import { Card } from "@mui/material"
+import { Card, CardContent } from "@mui/material"
 
 export const CountryCard = ({id,flag,name,population,region,capital}:CountryCardProps) => {
   return (
