ec37d049e0c2cc510082c97d4bf1bb844e8bec6d
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 16:40:39 -0600

    [FEAT] Cambios en el page CountriesDetail
    - Importando el componente MuiButton
    - importando el icono WestIcon
    - cambios en el html, establecemos la prop className para el css y establecemos un w-full, tamaño y color de bode

--- src/pages/CountriesDetail.tsx
+++ src/pages/CountriesDetail.tsx
@@ -1,7 +1,11 @@
+import { MuiButton } from "../components/MuiButton"
+import WestIcon from '@mui/icons-material/West';
 
 
 export const CountriesDetail = () => {
   return (
-    <div>CountriesDetail</div>
+    <div className="w-full border-2 border-red-300">
+
+    </div>
   )
 }
