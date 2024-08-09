7eae02cdd6a6b5ee8fff23b4acac2860aeff6288
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 21:19:31 -0600

    [FEAT] Cambios en el componente CountryCard
    - cambiando la definición de las fuentes, tamaño de fuentes
    - cambiando la estructura html para la descripción de los paises

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -13,20 +13,21 @@ export const CountryCard = ({flag,name,population,region,capital}:CountryCardPro
                 />
 
                 <CardContent className="dark:bg-DarkBlueBackground">
-                    <h1 className="font-sans-extrabold font-bold mb-1 dark:text-white">
+                    <h1 className="font-sans font-extrabold mb-1 dark:text-white">
                         {name}
                     </h1>
-                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                        Population:
-                        <span className="text-LightModeDarkGrayInput">{population}</span>
+                    {/* <p><strong className="font-sans font-semibold text-xs">Native Name. </strong>{item.name.nativeName?.nld.common}</p> */}
+                    <p className="text-LightModeDarkGrayInput font-sans font-semibold text-xs">
+                        <strong className="font-sans font-extrabold text-black text-xs dark:text-white">Population: </strong>
+                        {population}
                     </p>
-                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                        Region:
-                        <span className="text-LightModeDarkGrayInput">{region}</span>
+                    <p className="text-LightModeDarkGrayInput font-sans font-semibold text-xs">
+                        <strong className="font-sans font-extrabold text-black text-xs dark:text-white">Region: </strong>
+                        {region}
                     </p>
-                    <p className="font-sans-extrabold font-bold text-[12px] dark:text-white">
-                        Capital:
-                        <span className="text-LightModeDarkGrayInput">{capital}</span>
+                    <p className="text-LightModeDarkGrayInput font-sans font-semibold text-xs">
+                        <strong className="font-sans font-extrabold text-black text-xs dark:text-white">Capital :</strong>
+                        {capital}
                     </p>
                 </CardContent>
             </Link>
