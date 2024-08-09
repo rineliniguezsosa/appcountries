9b1414832bf44137ad392b7dcc62683b341d2e94
Rinel <rineliniguezsosa@gmail.com>
2024-08-08 21:25:43 -0600

    [FEAT] Estableciendo la clase dark en el elemento header del componente Nav
    - estableciendo el color de fondo usando el color customizado bg-DarkBlueBackground

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { DarkModeBtn } from "./DarkModeBtn"
 
 export const Nav = () => {
   return (
-    <header className="w-full shadow-md dark:border-yellow-600">
+    <header className="w-full shadow-md dark:border-yellow-600 bg-DarkBlueBackground">
         <nav className="w-full px-7 py-7  flex flex-row justify-between items-center">
             <h1 className="font-sans font-extrabold text-base dark:text-white">Where in the world?</h1>
 
