e49c97e47747ece4d32f0171d93419167ac75e5b
Rinel <rineliniguezsosa@gmail.com>
2024-08-09 22:39:00 -0600

    [FEAT] Estableciendo la prop value en el componente Select

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,8 +1,8 @@
 import { Select } from "@mui/material"
 import {MuiSelectProps} from '../types'
 
-export const MuiSelect = ({labelId, id, fullWidth, className, label, children,onChange }:MuiSelectProps) => {
+export const MuiSelect = ({labelId, id, fullWidth, className, label,value, children,onChange }:MuiSelectProps) => {
   return (
-    <Select labelId={labelId} id={id} fullWidth={fullWidth} className={className} label={label} onChange={onChange}>{children}</Select>
+    <Select labelId={labelId} id={id} fullWidth={fullWidth} className={className} label={label} value={value} onChange={onChange}>{children}</Select>
   )
 }
