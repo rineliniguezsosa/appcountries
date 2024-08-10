e1df3867762b6eed9e1ae9ba21d95a2762646564
Rinel <rineliniguezsosa@gmail.com>
2024-08-09 22:54:36 -0600

    [FEAT] Creando la carpeta utils y dentro de ella creando el archivo index.ts, el archivo index.ts contiene el array de objetos MenuItemList
    - este elemento servira para iterar sobre el componente que creé recientemente MuiMenuItem

---
+++ src/utils/index.ts
@@ -0,0 +1,33 @@
+export const MenuItemList = [
+    {
+        id:0,
+        value:'',
+        title: ''
+    },
+    {
+        id:1,
+        value:'Africa',
+        title: 'Africa'
+    },
+    {
+        id:2,
+        value:'Americas',
+        title: 'America'
+    },
+    {
+        id:3,
+        value:'Asia',
+        title: 'Asia'
+    },
+    {
+        id:4,
+        value:'Europe"',
+        title: 'Europe"'
+    },
+    {
+        id:4,
+        value:'Oceania"',
+        title: 'Oceania"'
+    },
+    
+]
