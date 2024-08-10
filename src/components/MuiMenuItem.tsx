937a2057b401b2bb07a93a9bb026bb2c77d13cd8
Rinel <rineliniguezsosa@gmail.com>
2024-08-10 14:50:15 -0600

    [FEAT] Modificando el componente MuiMenuItem
    - cambiamos la prop title por la envoltura children

--- src/components/MuiMenuItem.tsx
+++ src/components/MuiMenuItem.tsx
@@ -1,8 +1,10 @@
 import { MenuItem } from "@mui/material"
 import { MuiMenuItemProps } from '../types';
 
-export const MuiMenuItem = ({title,value,className}:MuiMenuItemProps) => {
+export const MuiMenuItem = ({children,value,className}:MuiMenuItemProps) => {
   return (
-    <MenuItem value={value} className={className}>{title}</MenuItem>
+    <MenuItem value={value} className={className}>
+        {children}
+    </MenuItem>
   )
 }
