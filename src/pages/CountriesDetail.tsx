b0874e65bfb0531871117b3d5a1ba6c8660e2f7d
Rinel <rineliniguezsosa@gmail.com>
2024-08-11 17:51:46 -0600

    [FEAT] Cambiando las props css en el breakpoint contenedor div
    - estableciendo cada una de las props css del breakpoint xl2
    - en xl2 el tamaño del width sera al 50%
    - en xl2 el padding-left tendra 20rem
    - estableciendo a los elementos div tamaño y color de borde

--- src/pages/CountriesDetail.tsx
+++ src/pages/CountriesDetail.tsx
@@ -66,10 +66,10 @@ export const CountriesDetail = () => {
       <article key={index} className="w-full flex flex-col border-2 border-red-400 xl2:flex-row">
         <img  className="w-full xl2:w-2/5" src={item.flags.svg} alt="flag" />
 
-        <div className="w-full py-10 border-2 border-blue-500 xl2:w-1/2 pl-20">
-            <h1 className="font-sans font-extrabold text-lg pb-5 dark:text-LightModeElements">{item.name.common}</h1>
+        <div className="w-full py-10 border-2 border-green-500 xl2:w-1/2 xl2:pl-20">
+            <h1 className="font-sans font-extrabold text-lg pb-5 border-2 border-blue-400 dark:text-LightModeElements">{item.name.common}</h1>
 
-            <div className="w-full border-2 border-blue-400 xl2:flex flex-row">
+            <div className="w-full xl2:flex flex-row">
               <div className="border-2 border-red-400 xl2:w-1/2">
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-2 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Native Name: </strong>{nativeName ? nativeName : ''}</p>
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-2 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Population: </strong>{item.population}</p>
@@ -77,7 +77,7 @@ export const CountriesDetail = () => {
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-2 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Sub Region: </strong>{item.subregion}</p>
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-7 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Capital: </strong>{item.capital}</p>
               </div>
-              <div className="border-2 border-black solid xl2:w-1/2">
+              <div className=" xl2:w-1/2">
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-2 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Top Level Domain: </strong>{item.tld}</p>
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-2 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Currencies: </strong>{currencies}</p>
                 <p className="font-sans font-semibold text-sm text-LightModeTextDarkBlue pb-7 dark:text-LightModeElements"><strong className="font-sans font-semibold text-sm">Languages: </strong>{languajes.join(' ')}</p>
