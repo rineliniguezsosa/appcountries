fd8d6f346336bb846f3a9cf191f95fb3ebd5dd08
Rinel <rineliniguezsosa@gmail.com>
2024-08-10 19:40:01 -0600

    [FEAT] Cambios en el componente MuiButton
    - cambiando la prop title por la prop children
    - estableciendo el children en la envoltura del componente Button

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@mui/material"
 import { MuiButtonProps } from "../types"
 
-export const MuiButton = ({variant,onClick,title,className,style,icon}:MuiButtonProps) => {
+export const MuiButton = ({variant,onClick,children,className,style,icon}:MuiButtonProps) => {
   return (
-    <Button variant={variant} onClick={onClick} style={style} startIcon={icon}>
-        <span className={className}>{title}</span>
+    <Button variant={variant} onClick={onClick} className={className} style={style} startIcon={icon}>
+        {children}
     </Button>
   )
 }
