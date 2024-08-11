25cae6d638e3cbbc37563b00eacd78d1989f807e
Rinel <rineliniguezsosa@gmail.com>
2024-08-10 19:57:09 -0600

    [FEAT] Estableciendo formato el componente MuiSelect para una perfecta lectura

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -3,6 +3,14 @@ import {MuiSelectProps} from '../types'
 
 export const MuiSelect = ({labelId, id, fullWidth, className,MenuProps, label,value, children,onChange }:MuiSelectProps) => {
   return (
-    <Select labelId={labelId} id={id} fullWidth={fullWidth} className={className} MenuProps={MenuProps} label={label} value={value} onChange={onChange}>{children}</Select>
+    <Select 
+      labelId={labelId} 
+      id={id} fullWidth={fullWidth}
+      className={className}
+      MenuProps={MenuProps} 
+      label={label}
+      value={value} 
+      onChange={onChange}>{children}
+      </Select>
   )
 }
