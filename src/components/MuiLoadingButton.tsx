10f8a6b3bdbd730b42da78af0af68bf24b4e383f
Rinel <rineliniguezsosa@gmail.com>
2024-08-11 20:39:50 -0600

    [FIX] Removiendo los espacios en blanco de las props en el componente MuiLoadingButton

--- src/components/MuiLoadingButton.tsx
+++ src/components/MuiLoadingButton.tsx
@@ -1,8 +1,8 @@
 import LoadingButton from '@mui/lab/LoadingButton';
 import { MuiLoadingButtonProps }from '../types';
 
-export const MuiLoadingButton = ({loading, variant, className, style, children}:MuiLoadingButtonProps) => {
-  return (
+export const MuiLoadingButton = ({loading,variant,className,style,children}:MuiLoadingButtonProps) => { 
+    return (
     <LoadingButton loading={loading} variant={variant} className={className} style={style}>{children}</LoadingButton>
   )
 }
