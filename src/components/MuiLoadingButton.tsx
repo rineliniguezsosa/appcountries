25b1a8f9e3dce946f99822cf17dfb3c339a05e66
Rinel <rineliniguezsosa@gmail.com>
2024-08-12 13:09:38 -0600

    [FEAT] Estableciendo formato al componente MuiLoadingButton para una mejor lectura de las props

--- src/components/MuiLoadingButton.tsx
+++ src/components/MuiLoadingButton.tsx
@@ -3,6 +3,14 @@ import { MuiLoadingButtonProps }from '../types';
 
 export const MuiLoadingButton = ({size,loading,variant,className,style,children}:MuiLoadingButtonProps) => { 
     return (
-    <LoadingButton size={size} loading={loading} variant={variant} className={className} style={style}>{children}</LoadingButton>
+    <LoadingButton
+      size={size} 
+      loading={loading} 
+      variant={variant} 
+      className={className} 
+      style={style}
+      >
+        {children}
+    </LoadingButton>
   )
 }
