151e18582b042559b44261846768c4a681e37c11
Rinel <rineliniguezsosa@gmail.com>
2024-08-12 13:07:19 -0600

    [FEAT] Estableciendo la prop size en el componente MuiLoadingButton
    - estableciendo la prop size en el componente LoadingButton

--- src/components/MuiLoadingButton.tsx
+++ src/components/MuiLoadingButton.tsx
@@ -1,8 +1,8 @@
 import LoadingButton from '@mui/lab/LoadingButton';
 import { MuiLoadingButtonProps }from '../types';
 
-export const MuiLoadingButton = ({loading,variant,className,style,children}:MuiLoadingButtonProps) => { 
+export const MuiLoadingButton = ({size,loading,variant,className,style,children}:MuiLoadingButtonProps) => { 
     return (
-    <LoadingButton loading={loading} variant={variant} className={className} style={style}>{children}</LoadingButton>
+    <LoadingButton size={size} loading={loading} variant={variant} className={className} style={style}>{children}</LoadingButton>
   )
 }
