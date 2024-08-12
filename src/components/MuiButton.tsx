b320d5cde78ef5a7e7a7ad2bf44fd1fa909d59fd
Rinel <rineliniguezsosa@gmail.com>
2024-08-12 13:04:14 -0600

    [FEAT] Estableciendo la prop size en el componente MuiButton
    - estableciendo formato al componente MuiButton para una mejor lectura

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@mui/material"
 import { MuiButtonProps } from "../types"
 
-export const MuiButton = ({variant,onClick,children,className,style,icon}:MuiButtonProps) => {
+export const MuiButton = ({variant,onClick,children,className,style,icon,size}:MuiButtonProps) => {
   return (
-    <Button variant={variant} onClick={onClick} className={className} style={style} startIcon={icon}>
+    <Button 
+      variant={variant} 
+      onClick={onClick} 
+      className={className}
+      style={style}
+      startIcon={icon} 
+      size={size}
+      >
         {children}
     </Button>
   )
