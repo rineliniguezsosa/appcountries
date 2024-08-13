24681586bcf1c404d985821d8d2de48a9b31b42f
Rinel <rineliniguezsosa@gmail.com>
2024-08-12 21:24:45 -0600

    [FEAT] Estableciendo la prop style en el componente MuiMenuItem
    - estableciendo la prop style en el componente Select

--- src/components/MuiMenuItem.tsx
+++ src/components/MuiMenuItem.tsx
@@ -1,9 +1,9 @@
 import { MenuItem } from "@mui/material"
 import { MuiMenuItemProps } from '../types';
 
-export const MuiMenuItem = ({children,value,className}:MuiMenuItemProps) => {
+export const MuiMenuItem = ({children,value,className,style}:MuiMenuItemProps) => {
   return (
-    <MenuItem value={value} className={className}>
+    <MenuItem  value={value} className={className} style={style}>
         {children}
     </MenuItem>
   )
