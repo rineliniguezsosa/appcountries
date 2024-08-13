84fbbae3024c13da8614b0e6a84d47e705de6c1e
Rinel <rineliniguezsosa@gmail.com>
2024-08-12 21:23:10 -0600

    [FEAT] Estableciendo formato en el componente MuSelect
    - estableciendo la prop sx en el componente MuiSelect
    - estableciendo el argumento fullWidth como false en caso de que no se especifique

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,16 +1,20 @@
 import { Select } from "@mui/material"
 import {MuiSelectProps} from '../types'
 
-export const MuiSelect = ({labelId, id, fullWidth, className,MenuProps, label,value, children,onChange }:MuiSelectProps) => {
+export const MuiSelect = ({labelId,id,fullWidth = false,className,MenuProps,sx,label,value,children,onChange }:MuiSelectProps) => {
   return (
     <Select 
       labelId={labelId} 
-      id={id} fullWidth={fullWidth}
+      id={id} 
+      fullWidth={fullWidth}
       className={className}
       MenuProps={MenuProps} 
       label={label}
       value={value} 
-      onChange={onChange}>{children}
-      </Select>
+      onChange={onChange}
+      sx={sx}
+    >
+    {children}
+    </Select>
   )
 }
