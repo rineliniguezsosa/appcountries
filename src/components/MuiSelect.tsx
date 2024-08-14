d19c5e38bc8e43de830ee4c4d8050137f1889737
Rinel <rineliniguezsosa@gmail.com>
2024-08-13 18:52:25 -0600

    [FEAT] Estableciendo la prop  IconComponent en el componente Select
    - desestructurando la prop  IconComponent

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,12 +1,13 @@
 import { Select } from "@mui/material"
 import {MuiSelectProps} from '../types'
 
-export const MuiSelect = ({labelId,id,fullWidth,className,MenuProps,sx,label,value,children,onChange }:MuiSelectProps) => {
+export const MuiSelect = ({labelId,id,fullWidth,IconComponent,className,MenuProps,sx,label,value,children,onChange }:MuiSelectProps) => {
   return (
     <Select 
       labelId={labelId} 
       id={id} 
       fullWidth={fullWidth}
+      IconComponent={IconComponent}
       className={className}
       MenuProps={MenuProps} 
       label={label}
