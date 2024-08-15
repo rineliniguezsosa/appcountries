5ae6bcd536cede59c78d0db978e501ccc1f2c33f
Rinel <rineliniguezsosa@gmail.com>
2024-08-15 13:52:29 -0600

    [FEAT] Importando el componente Link de react-router-dom en el componente Nav
    - envolvemos el elemento h1 por el componente Link de react-router
    - establecemos al componente Link la prop to para navegar al usuario a la ruta principal de la aplicación

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
 import { DarkModeBtn } from "./DarkModeBtn"
-
 
 export const Nav = () => {
   return (
     <header className="w-full shadow-md bg-LightModeElements dark:bg-DarkBlueBackground">
         <nav className="w-full px-7 py-7  flex flex-row justify-between items-center">
-            <h1 className="font-sans font-extrabold text-base dark:text-white">Where in the world?</h1>
+            <Link to="/">
+              <h1 className="font-sans font-extrabold text-base dark:text-white">Where in the world?</h1>
+            </Link>
 
             <DarkModeBtn/>
         </nav>
