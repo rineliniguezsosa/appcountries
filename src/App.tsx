8fb6b72fcf8f178f95ed690eb3276448d63259c9
Rinel <rineliniguezsosa@gmail.com>
2024-08-15 21:07:26 -0600

    [FEAT] Estableciendo validación de ruta en el componente App
    - Importando el componente Navigate de react router dom
    - estableciendo la ruta que acepta cualquier ruta donde devolveremos el componente Navigate con la ruta principal de la aplicación
    - realizando esta pequeña operación pero importante estamos validando la redirección de nuestras rutas conocidas unicamente
    - removiendo la linea 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route,Navigate } from 'react-router-dom';
 import { Countries } from './pages/Countries';
 import { CountriesDetail } from './pages/CountriesDetail';
 import { Nav } from './components/Nav';
 import './App.css'
 
 function App() {
-
   return (
     <main>
       <Nav/>
         <Routes>
           <Route path='/' element={<Countries/>} />
           <Route path='/countries/detail/:name?' element={<CountriesDetail/>} />
+          <Route path='/*' element={<Navigate to="/"/>}/>
         </Routes>
     </main>
   )
