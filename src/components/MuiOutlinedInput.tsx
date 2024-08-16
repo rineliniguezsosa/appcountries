d045400e4cbc614c065957cabf81195272ba9c9f
Rinel <rineliniguezsosa@gmail.com>
2024-08-16 11:54:06 -0600

    [FEAT] Estableciendo la prop sx en el componente OutLinedInput
    - estableciendo el argumento sx en el componente MuiOutlinedInput
    - sx sirve para establecer css en los componentes de matreial ui
    - sx es una propiedad que nos permite establecer estilos css

--- src/components/MuiOutlinedInput.tsx
+++ src/components/MuiOutlinedInput.tsx
@@ -2,7 +2,7 @@ import { OutlinedInput } from "@mui/material"
 import { MuiOutlinedInputProps } from "../types"
 
 
-export const MuiOutlinedInput = ({id,fullWidth = false,type,startAdornment,label,className,onChange}:MuiOutlinedInputProps) => {
+export const MuiOutlinedInput = ({id,fullWidth = false,type,startAdornment,label,className,onChange,sx}:MuiOutlinedInputProps) => {
   return (
     <OutlinedInput 
       id={id}
@@ -11,6 +11,7 @@ export const MuiOutlinedInput = ({id,fullWidth = false,type,startAdornment,label
       startAdornment={startAdornment} 
       label={label} 
       className={className}
+      sx={sx}
       onChange={onChange}
       classes={{
         input: 'font-sans font-semibold text-sm text-black dark:text-white', // Aplica la clase directamente al input
