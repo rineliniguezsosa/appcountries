b9500a83dd0e0c9e6804d7b8a0e4cf1bbb3a7bb6
Rinel <rineliniguezsosa@gmail.com>
2024-08-17 11:56:19 -0600

    [FEAT] Guardando la url del repositorio en la opción base del archivo vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/appcountries/'
+  base:'https://github.com/rineliniguezsosa/appcountries'
 })
